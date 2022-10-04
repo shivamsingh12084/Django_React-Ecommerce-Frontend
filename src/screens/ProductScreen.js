571bd7cac4f37e028f7e9fd6dcae23ee379ec23d
shivamsingh12084 <shivamsingh12084@gmail.com>
2022-10-04 22:49:51 +0530

    Fetching data from backend using useState, useEffect and axion

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import { Link, useParams} from 'react-router-dom'
+import axios from 'axios'
 import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
 
 import Rating from '../components/Rating'
@@ -8,7 +9,18 @@ import products from '../products'
 
 function ProductScreen() {
     const {id} = useParams()
-    const product = products.find((p) => p._id == id)
+
+
+    const [product, setProduct] = useState([])
+   
+    useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+
+  }, []) 
   return (
     <div>
       <Link to='/' className='btn btn-light my-3'>Go Back</Link>
